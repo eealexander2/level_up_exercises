0ddc8fef09d4d63f31d6f09579887fcdcb52b316
Elizabeth Alexander <eealexander2@gmail.com>
2016-08-17 16:09:26 -0500

    Commit changes

--- robot_name/robot_name.rb
+++ robot_name/robot_name.rb
@@ -1,27 +1,26 @@
+class NameCollisionError < RuntimeError; end
+class NameDuplicationError < RuntimeError; end
+
 class Robot
   attr_accessor :name
 
   def initialize(args = {})
-    if args[:name_generator]
-      @name = args[:name_generator]
-    else
-      generate_char = Array.new(2) { ("A".."Z").to_a.sample }.join
-      generate_num =  Array.new(3) { rand(10) }.join
-      @name = "#{generate_char}#{generate_num}"
-    end
+    @registry ||= []
+    @name = args[:name_generator] || name_generator
+    raise NameCollisionError, 'There was a problem generating the robot name!' unless @name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
+    raise NameDuplicationError, 'This name already exists!' if @registry.include?(@name)
+    @registry << @name
   end
 
-  def check_error
-    unless @name =~ /[[:alpha:]]{2}[[:digit:]]{3}/
-      raise "There was a problem generating the robot name!"
-    end
+  def name_generator
+    char = Array.new(2) { ("A".."Z").to_a.sample }.join
+    num =  Array.new(3) { rand(10) }.join
+    @name = "#{char}#{num}"
   end
 end
 
 robot = Robot.new
-robot.check_error
 puts "My pet robot's name is #{robot.name}, but we usually call him sparky."
-
 # Errors!
 # generator = -> { 'AA111' }
 # Robot.new(name_generator: generator)
